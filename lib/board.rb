1f955c66c1a00afd263cd90ad53f0d472336748d
cheikh <cheikh.kebe.01@gmail.com>
2021-05-03 09:26:15 +0200

    maj

--- lib/board.rb
+++ lib/board.rb
@@ -13,12 +13,12 @@ class Board
     puts "--|---|--"
     puts "#{@board[6..8].join(" | ")}"
   end
-#module qui va
+  #module qui va definir le tour de jeu 
   def play_turn(symbol)
     case_number = gets.chomp().to_i
     @board = @board.each_index.map { |e| e == case_number && @board[e] != "X" && @board[e] != "O" ? @board[e] = symbol : @board[e] }  
   end
-#module qui defini s'il y a victoire
+  #module qui defini les conditions de victioire
   def victory?
     if (@board[0] == @board[1] && @board[0] == @board[2] || @board[3] == @board[4] && @board[3] == @board[5] || @board[6] == @board[7] && @board[6] == @board[8] || @board[0] == @board[3] && @board[0] == @board[6] || @board[1] == @board[4] && @board[1] == @board[7] || @board[2] == @board[5] && @board[2] == @board[8] || @board[0] == @board[4] && @board[0] == @board[8] || @board[2] == @board[4] && @board[2] == @board[6])
       return true
